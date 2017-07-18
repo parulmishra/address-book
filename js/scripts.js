a5a90c96d64b42fc244a158a2522411ad56532df
parul mishra dubedy <parul.mishra78@gmail.com>
2017-07-18 11:39:02 -0700

    This commit will update the address-book.html added form-control, form-group, select tags and updated the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,34 @@
-function contact(firstname, lastname, phonenumber,city)
+function Contact(firstname, lastname, phoneNumber,gender,country,city, houseNumber, street)
 {
+
   this.firstname=firstname;
   this.lastname=lastname;
-  this.phonenumber=phonenumber;
+  this.phoneNumber=phoneNumber;
+  this.gender=gender;
+  this.country=country;
   this.city=city;
+  this.houseNumber=houseNumber;
+  this.street=street;
 }
+//Here we are nit making a prototype function to access the phone number inputted by user because we want to check the validation of the number before the contact object is created.
+function checkPhoneNumberLength(input)
+{
+  console.log(input.toString().length);
+  if( input.toString().length<10 || input.toString().length>10 || isNaN(input) || input == "")
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+$(document).ready(function () {
+  var phoneNumber = $("input#phone").val();
+  if (checkPhoneNumberLength(phoneNumber) {
+    newContact = new COntact(lksjdflkjsdflkjf, phoneNumber)
+  }
+  else
+  {
+  }
+});
